7a2bd8b127716faafabc9e16fbb8a5953c4796dd
Dwiky Rizky Ananditya <kyzsuki@gmail.com>
2025-01-07 18:19:29 +0700

    improve debug ouput
    Signed-off-by: Dwiky Rizky Ananditya <kyzsuki@gmail.com>

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -7,14 +7,15 @@ const debugCommand = createCommand(
     description: 'send the json',
   },
   async ctx => {
-    let jsonStr: any = JSON.stringify(ctx.message, null, 4);
+    let jsonStr = JSON.stringify(ctx.message, null, 4);
 
     let replyparam: ReplyParameters = {
       message_id: ctx.message?.message_id!,
     };
 
-    await ctx.reply(jsonStr, {
+    await ctx.reply(`\`\`\`json\n${jsonStr}\n\`\`\``, {
       reply_parameters: replyparam,
+      parse_mode: 'MarkdownV2',
     });
   }
 );
